29e9c03f99c97877a0db15aa2aa788b58beb758e
gig5 <karunakar.lakkireddy@in.tesco.com>
2018-06-17 19:42:35 +0530

    changing Autocloseable streams

--- src/main/java/org/target/problem3/casestudy/service/ProcessRequests.java
+++ src/main/java/org/target/problem3/casestudy/service/ProcessRequests.java
@@ -18,73 +18,60 @@ import org.target.problem3.casestudy.model.RequestModel;
 
 public class ProcessRequests {
 
-    public static void processRequests(List<RequestModel> listOfRequestModel,
-	    Map<AssetType, List<InventoryModel>> mapOfInvetory) throws IOException {
+	public static void processRequests(List<RequestModel> listOfRequestModel,
+			Map<AssetType, List<InventoryModel>> mapOfInvetory) throws IOException {
 
-	for (RequestModel requestModel : listOfRequestModel) {
-	    List<InventoryModel> listOfInventoryModel = mapOfInvetory.get(requestModel.getAssetType());
-	    InventoryModel inventoryModel = listOfInventoryModel.stream()
-		    .filter(inventory -> inventory.getLocation().equals(requestModel.getLocation())).findFirst()
-		    .orElse(null);
-	    if(inventoryModel!=null){
-		if(inventoryModel.getAvailable()>=1){
-		    updateInventory(inventoryModel);
-		    requestModel.setRequestStatus(RequestStatus.FULLFILLED);
-		    updateRequest(requestModel);
-		}else{
-		    requestModel.setRequestStatus(RequestStatus.LOWINVERNTORY);
-		    updateRequest(requestModel);
+		for (RequestModel requestModel : listOfRequestModel) {
+			List<InventoryModel> listOfInventoryModel = mapOfInvetory.get(requestModel.getAssetType());
+			InventoryModel inventoryModel = listOfInventoryModel.stream()
+					.filter(inventory -> inventory.getLocation().equals(requestModel.getLocation())).findFirst()
+					.orElse(null);
+			if (inventoryModel != null) {
+				if (inventoryModel.getAvailable() >= 1) {
+					updateInventory(inventoryModel);
+					requestModel.setRequestStatus(RequestStatus.FULLFILLED);
+					updateRequest(requestModel);
+				} else {
+					requestModel.setRequestStatus(RequestStatus.LOWINVERNTORY);
+					updateRequest(requestModel);
+				}
+			} else {
+				System.out.println("Request no" + requestModel.getRequestNumber() + "cannot be processed");
+			}
+
 		}
-	    }else{
-		System.out.println("Request no"+requestModel.getRequestNumber()+ "cannot be processed");
-	    }
 
 	}
 
-    }
-    
-    
-    private static void updateRequest(RequestModel requestModel) throws IOException {
-	FileOutputStream fileOutputStream =null;
-	FileInputStream fileInputStream=null;
-	try{
-	 fileInputStream= new FileInputStream("F://AssetRequest.xlsx");
-	Workbook workbook = new XSSFWorkbook(fileInputStream);
-	Sheet sheet = workbook.getSheetAt(0);
-	Row row = sheet.getRow(requestModel.getRowNumber());
-	Cell cell = row.getCell(3);
-	cell.setCellValue(requestModel.getRequestStatus().name());
-	
-	fileOutputStream= new FileOutputStream("F://AssetRequest.xlsx");
-	workbook.write(fileOutputStream);
-	}finally{
-	    if(fileInputStream!=null)
-	    fileInputStream.close();
-	    if(fileOutputStream!=null)
-		fileOutputStream.close();
-	}
-    }
+	private static void updateRequest(RequestModel requestModel) throws IOException {
 
+		try (FileInputStream fileInputStream = new FileInputStream("F://AssetRequest.xlsx");
+				FileOutputStream fileOutputStream = new FileOutputStream("F://AssetRequest.xlsx");) {
 
-    private static void updateInventory(InventoryModel inventoryModel) throws IOException{
-	FileInputStream fileInputStream= null;
-	FileOutputStream fileOutputStream=null;
-	try{
-	fileInputStream = new FileInputStream("F://AssetRequest.xlsx");
-	Workbook workbook = new XSSFWorkbook(fileInputStream);
-	Sheet sheet = workbook.getSheetAt(1);
-	Row row = sheet.getRow(inventoryModel.getRowNumber());
-	Cell cell =row.createCell(1);
-	cell.setCellValue(inventoryModel.getAvailable().doubleValue()-1.0);
-	fileOutputStream = new FileOutputStream("F://AssetRequest.xlsx");
-	workbook.write(fileOutputStream);
-	
-	}finally{
-	    if(fileOutputStream!=null)
-	    fileOutputStream.close();
-	    if(fileInputStream!=null)
-	    fileInputStream.close();
+			Workbook workbook = new XSSFWorkbook(fileInputStream);
+			Sheet sheet = workbook.getSheetAt(0);
+			Row row = sheet.getRow(requestModel.getRowNumber());
+			Cell cell = row.getCell(3);
+			cell.setCellValue(requestModel.getRequestStatus().name());
+
+			workbook.write(fileOutputStream);
+		}
 	}
-    }
-    
+
+	private static void updateInventory(InventoryModel inventoryModel) throws IOException {
+
+		try (FileInputStream fileInputStream = new FileInputStream("F://AssetRequest.xlsx");
+				FileOutputStream fileOutputStream = new FileOutputStream("F://AssetRequest.xlsx");) {
+
+			Workbook workbook = new XSSFWorkbook(fileInputStream);
+			Sheet sheet = workbook.getSheetAt(1);
+			Row row = sheet.getRow(inventoryModel.getRowNumber());
+			Cell cell = row.createCell(1);
+			cell.setCellValue(inventoryModel.getAvailable().doubleValue() - 1.0);
+
+			workbook.write(fileOutputStream);
+
+		}
+	}
+
 }
--- src/main/java/org/target/problem3/casestudy/service/ReadAllRequests.java
+++ src/main/java/org/target/problem3/casestudy/service/ReadAllRequests.java
@@ -18,70 +18,61 @@ import org.target.problem3.casestudy.model.RequestModel;
 
 public class ReadAllRequests {
 
-    
-    public static List<RequestModel> getAllRequests() throws IOException {
-	List<RequestModel> listOfRequestModels = new ArrayList<>();
-	FileInputStream fileInputStream =null;
-	try{
-	fileInputStream= new FileInputStream("F://AssetRequest.xlsx");
-	Workbook workbook = new XSSFWorkbook(fileInputStream);
-	Sheet sheet = workbook.getSheetAt(0);
-	Iterator<Row> iterator = sheet.rowIterator();
-	iterator.next();
-	while (iterator.hasNext()) {
-	    String[] data = new String[5];
-	    Row row = iterator.next();
-	    Iterator<Cell> cellIterator = row.cellIterator();
-	    int i = 0;
-	    while (cellIterator.hasNext()) {
-		Cell cell = cellIterator.next();
-		if (cell.getCellType() == Cell.CELL_TYPE_STRING)
-		    data[i] = cell.getStringCellValue();
-		else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC)
-		    data[i] = Double.valueOf(cell.getNumericCellValue()).toString();
-		i++;
-	    }
-	    RequestModel requestModel = new RequestModel(Double.valueOf(data[0]).longValue(),
-		    AssetType.valueOf(data[1]), Double.valueOf(data[2]).longValue(), RequestStatus.valueOf(data[3]), data[4],row.getRowNum());
-	    listOfRequestModels.add(requestModel);
-	}
-	}finally{
-	    if(fileInputStream!=null)
-	fileInputStream.close();
-	}
-	return listOfRequestModels;
+	public static List<RequestModel> getAllRequests() throws IOException {
+		List<RequestModel> listOfRequestModels = new ArrayList<>();
+		try (FileInputStream fileInputStream = new FileInputStream("F://AssetRequest.xlsx");) {
+			Workbook workbook = new XSSFWorkbook(fileInputStream);
+			Sheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = sheet.rowIterator();
+			iterator.next();
+			while (iterator.hasNext()) {
+				String[] data = new String[5];
+				Row row = iterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				int i = 0;
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					if (cell.getCellType() == Cell.CELL_TYPE_STRING)
+						data[i] = cell.getStringCellValue();
+					else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC)
+						data[i] = Double.valueOf(cell.getNumericCellValue()).toString();
+					i++;
+				}
+				RequestModel requestModel = new RequestModel(Double.valueOf(data[0]).longValue(),
+						AssetType.valueOf(data[1]), Double.valueOf(data[2]).longValue(), RequestStatus.valueOf(data[3]),
+						data[4], row.getRowNum());
+				listOfRequestModels.add(requestModel);
+			}
+		}
+		return listOfRequestModels;
 
-    }
-    
-    
-    public static List<InventoryModel> getAllInventory() throws IOException{
-	List<InventoryModel> listOfInventoryModel = new ArrayList<>();
-	FileInputStream fileInputStream =null;
-	try{
-	fileInputStream= new FileInputStream("F://AssetRequest.xlsx");
-	Workbook workbook = new XSSFWorkbook(fileInputStream);
-	Sheet sheet = workbook.getSheetAt(1);
-	Iterator<Row> iterator = sheet.rowIterator();
-	iterator.next();
-	while(iterator.hasNext()){
-	    String[] data = new String[3];
-	    Row row = iterator.next();
-	    Iterator<Cell> cellIterator = row.cellIterator();
-	    int i = 0;
-	    while(cellIterator.hasNext()){
-		Cell cell = cellIterator.next();
-		if(cell.getCellType()==Cell.CELL_TYPE_STRING)
-		data[i] = cell.getStringCellValue();
-		else if(cell.getCellType()==Cell.CELL_TYPE_NUMERIC)
-		    data[i] = Double.valueOf(cell.getNumericCellValue()).toString();
-		i++;
-	    }
-	    InventoryModel inventoryModel = new InventoryModel(AssetType.valueOf(data[0]), Double.valueOf(data[1]).longValue(), data[2],row.getRowNum());
-	    listOfInventoryModel.add(inventoryModel);
 	}
-	}finally{
-	fileInputStream.close();
+
+	public static List<InventoryModel> getAllInventory() throws IOException {
+		List<InventoryModel> listOfInventoryModel = new ArrayList<>();
+		try (FileInputStream fileInputStream = new FileInputStream("F://AssetRequest.xlsx");) {
+			Workbook workbook = new XSSFWorkbook(fileInputStream);
+			Sheet sheet = workbook.getSheetAt(1);
+			Iterator<Row> iterator = sheet.rowIterator();
+			iterator.next();
+			while (iterator.hasNext()) {
+				String[] data = new String[3];
+				Row row = iterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				int i = 0;
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+					if (cell.getCellType() == Cell.CELL_TYPE_STRING)
+						data[i] = cell.getStringCellValue();
+					else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC)
+						data[i] = Double.valueOf(cell.getNumericCellValue()).toString();
+					i++;
+				}
+				InventoryModel inventoryModel = new InventoryModel(AssetType.valueOf(data[0]),
+						Double.valueOf(data[1]).longValue(), data[2], row.getRowNum());
+				listOfInventoryModel.add(inventoryModel);
+			}
+		}
+		return listOfInventoryModel;
 	}
-	return listOfInventoryModel;
-    }
 }
